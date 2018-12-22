8c8d10e501781379c32cbb80ddd81bf8ac85675d
DeliciousShoesHD <bizjaktomaz72@gmail.com>
2018-12-22 18:17:15 +0100

    Dodana 4. naloga za 1. skupino

---
+++ Skupina1Naloga4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skupina1Naloga4
+{
+    class Program
+    {
+        static int max = 250;
+        static int kazalec = 0;
+        static int zacetek = max;
+        static int hitrost = 0;
+
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            while(true)
+            {
+                hitrost = rnd.Next(0, 301);
+                Console.WriteLine(Premik(hitrost) + " , " + hitrost + " , " + kazalec);
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+
+        static int Premik(int hitrost)
+        {
+            if(zacetek > 0)
+            {
+                zacetek--;
+                return -1;
+            }
+
+            if(hitrost > max)
+            {
+                hitrost = max;
+            }
+
+            int premik;
+            if (hitrost > kazalec) premik = 1;
+            else if (hitrost < kazalec) premik = -1;
+            else premik = 0;
+
+            kazalec += premik;
+            return premik;
+        }
+    }
+}
