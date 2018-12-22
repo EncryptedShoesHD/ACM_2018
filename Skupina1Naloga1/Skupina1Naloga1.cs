9c7fa6523db7cdddd2766f8bfd91f0c966d02ce3
DeliciousShoesHD <bizjaktomaz72@gmail.com>
2018-12-22 13:54:12 +0100

    Dodana 1. naloga za 1. skupino.

---
+++ Skupina1Naloga1/Skupina1Naloga1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S1Naloga1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PoisciVse(30, 50);
+            Console.ReadKey();
+        }
+
+        static void PoisciVse(int a, int b)
+        {
+            for (; a <= b; a++)
+            {
+                int i = a;
+                while (i != 1)
+                {
+                    if (i % 2 == 0) i /= 2;
+                    else i = 3 * i + 1;
+                    if (i >= 9232)
+                    {
+                        Console.Write(a + " ");
+                    }
+                }
+            }
+        }
+    }
+}
